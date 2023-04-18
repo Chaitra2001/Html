8968fedf432709693b85baec39e95fbb222ffdf8
Chaitra2001 <chaitrabommanalli1234@gmail.com>
2023-04-18 02:04:00 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var age=prompt("enter your age");
+const data =(age<=18?"not allowed to club":"allowed to club")
+console.log(data);
+
+if(age<18){
+    console.log("you are not eligible to vote id");
+    else if(age )
+}
+for(i=1;i<=10;i++){
+    console.log(i);
+}
